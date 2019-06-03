fc3739cc92c84baa7abc5f8903187397fad1c226
12312312312 <vladtsibizov@yandex.ru>
2019-06-03 13:31:37 +0300

    .

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -25,14 +25,17 @@ void MainWindow::initializeGL()
     glEnable(GL_LIGHT0);
     glEnable(GL_COLOR_MATERIAL);
 
-    GLfloat light_ambient[] =  { 0.0, 0.0, 0.0, 1.0 };
+    GLfloat light_ambient[] =  { 0.2, 0.2, 0.2, 1.0 };
     GLfloat light_diffuse[] =  { 1.0, 1.0, 1.0, 1.0 };
     GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
     GLfloat light_position[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat mat_shininess[]={50.0};
+    GLfloat mat_shininess[]={50};
 
     glMaterialfv(GL_FRONT, GL_SPECULAR, light_specular);
     glMaterialfv(GL_FRONT,GL_SHININESS, mat_shininess);
+    glMaterialfv(GL_FRONT,GL_AMBIENT, light_ambient);
+    glMaterialfv(GL_FRONT,GL_DIFFUSE, light_diffuse);
+
     glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
     glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
