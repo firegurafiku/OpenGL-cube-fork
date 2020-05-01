9dddfe61cf07e800fd66fdd46094f9a53cdc1a60
Vladislav Tsibizov <vladtsibizov@gmail.com>
2020-05-01 03:08:18 +0300

    slider and spinBox

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -6,8 +6,16 @@ MainWidget::MainWidget(QWidget *parent) :
     ui(new Ui::MainWidget)
 {
     ui->setupUi(this);
+
     connect(ui->horizontalSlider, SIGNAL(valueChanged(int)),
                 ui->openGLWidget, SLOT(setDepth(int)));
+
+    connect(ui->horizontalSlider, SIGNAL(valueChanged(int)),
+            this, SLOT(sliderChanged(int)));
+
+    connect(ui->spinBox, SIGNAL(valueChanged(int)),
+                this, SLOT(depthSpinBoxChanged(int)));
+
     connect(ui->openGLWidget, SIGNAL(setMaxDepthSignal(int)),
                 this, SLOT(setMaxDepth(int)));
 }
@@ -20,4 +28,15 @@ MainWidget::~MainWidget()
 void MainWidget::setMaxDepth(int d)
 {
     this->ui->horizontalSlider->setMaximum(d);
+    this->ui->spinBox->setMaximum(d);
+}
+
+void MainWidget::depthSpinBoxChanged(int d)
+{
+    ui->horizontalSlider->setValue(d);
+}
+
+void MainWidget::sliderChanged(int d)
+{
+    ui->spinBox->setValue(d);
 }
--- mainwidget.h
+++ mainwidget.h
@@ -2,6 +2,7 @@
 #define MAINWIDGET_H
 
 #include <QWidget>
+#include <stdexcept>
 
 namespace Ui {
 class MainWidget;
@@ -17,6 +18,8 @@ private:
     Ui::MainWidget *ui;
 private slots:
     void setMaxDepth(int d);
+    void depthSpinBoxChanged(int d);
+    void sliderChanged(int d);
 };
 
 #endif // MAINWIDGET_H
